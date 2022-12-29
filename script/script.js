9243cbdc8633e824a715884f6a0921badbdf6e48
SilverSol <ivan.soloviov@nure.ua>
2022-12-29 11:25:07 +0200

    First commit

---
+++ script/script.js
@@ -0,0 +1,362 @@
+//alert("hello user");
+let questionsE=[{
+    'question':"Какой продукт в разных странах называют папиной бородой и бабушкиными волосами?",
+    'answerA':"A Сахарную вату",
+    'answerB':"B Пицца ",
+    'answerC':"С Заливніе яблоки",
+    'answerD':"D Маринованую Капусту",
+    'right':"A",
+    'fifty':"AC",
+},{
+    'question':"Какое действие иногда вынуждает совершить посетитель по отношению к коррумпированному чиновнику?",
+    'answerA':"A Подмазать",
+    'answerB':"B Смазать",
+    'answerC':"C Вмазать",
+    'answerD':"D Намазать",
+    'right':"A",
+    'fifty':"AC",
+},{
+    'question':"Как называется распространенная детская игра?",
+    'answerA':"А Пираты-разбойники",
+    'answerB':"B Соловьи-разбойники",
+    'answerC':"С Казаки-разбойники",
+    'answerD':"D Орки-разбойники",
+    'right':"C",
+    'fifty':"CB",
+},{
+    'question':"С какого тура, обычно начинаются выборы президента страны?",
+    'answerA':"А С Премьерного",
+    'answerB':"B С Первого",
+    'answerC':"С С Малого",
+    'answerD':"D С Пробного",
+    'right':"B",
+    'fifty':"BA",
+},{
+    'question':"Как звали друга Карлсона?",
+    'answerA':"А Вова",
+    'answerB':"B Ганс",
+    'answerC':"С Глупыш",
+    'answerD':"D Малыш",
+    'right':"D",
+    'fifty':"DB",
+},{
+    'question':"Какое астрономическое явление жители Земли могут наблюдать раз в 75–76 лет?",
+    'answerA':"А Перевыборы в России",
+    'answerB':"В Пpилет метеорита",
+    'answerC':"С Появление кометы Галлея",
+    'answerD':"D Падение атомной бомбы",
+    'right':"C",
+    'fifty':"CA",
+}];
+let questionsM=[{
+    'question':"Кто был первым военным министром Российской империи?",
+    'answerA':"A Аракчеев",
+    'answerB':"B Коновницын",
+    'answerC':"С Вязмитинов",
+    'answerD':"D Барклай-де-Толли",
+    'right':"C",
+    'fifty':"CD",
+},{
+    'question':"Что Шекспир назвал «вкуснейшим из блюд в земном пиру»?",
+    'answerA':"A Опьянение",
+    'answerB':"B Любовь",
+    'answerC':"C Уединение",
+    'answerD':"D Сон",
+    'right':"D",
+    'fifty':"DA",
+},{
+    'question':"Кто из этих философов в 1864 году написал музыку на стихи А. С. Пушкина «Заклинание» и «Зимний вечер»?",
+    'answerA':"А Юнг",
+    'answerB':"B Ницше",
+    'answerC':"С Шопенгауэр",
+    'answerD':"D Гегель",
+    'right':"B",
+    'fifty':"BA",
+},{
+    'question':"Возле какой горы-вулкана был впервые найден драгоценный камень танзанит?",
+    'answerA':"А Килиманджаро",
+    'answerB':"B Фудзияма",
+    'answerC':"С Горы Кавказа",
+    'answerD':"D Джамалунмгма",
+    'right':"A",
+    'fifty':"AB",
+},{
+    'question':"Какой головной убор был во время бала на Татьяне Лариной, героине романа «Евгений Онегин»?",
+    'answerA':"А Синий платок   ",
+    'answerB':"B Малиновый берет",
+    'answerC':"С Голубой плащ",
+    'answerD':"D Черній зонт",
+    'right':"B",
+    'fifty':"BA",
+},{
+    'question':"Кем приходились друг другу толстый и тонкий из одноимённого рассказа Чехова?",
+    'answerA':"А Коллегами",
+    'answerB':"B Братьями",
+    'answerC':"С Сослуживцами",
+    'answerD':"D Одноклассниками",
+    'right':"D",
+    'fifty':"DB",
+},{
+    'question':"Название какого алкогольного напитка переводится с немецкого как «полынь»?",
+    'answerA':"А Шнапс",
+    'answerB':"В Сквош",
+    'answerC':"С Вермут",
+    'answerD':"D Шампанское",
+    'right':"C",
+    'fifty':"CA",
+}];
+let questionsH=[{
+    'question':"В какое созвездие входит звезда Шаула, название которой переводится как «поднятый хвост»?",
+    'answerA':"A Пояс Ориона",
+    'answerB':"B Медведица",
+    'answerC':"С Скорпион",
+    'answerD':"D Крест",
+    'right':"C",
+    'fifty':"CD",
+},{
+    'question':"Скольким каратам соответствует чистое золото?",
+    'answerA':"A 30-каратное",
+    'answerB':"B 23-каратное",
+    'answerC':"C 25-каратное",
+    'answerD':"D 24-каратное",
+    'right':"D",
+    'fifty':"DA",
+},{
+    'question':"Как называется красная тряпка в руках у матадора?",
+    'answerA':"А Кастера",
+    'answerB':"B Мулета",
+    'answerC':"С Муньета",
+    'answerD':"D Буэна",
+    'right':"B",
+    'fifty':"BA",
+},{
+    'question':"В каком из этих напитков, по мнению дегустаторов, преобладает оттенок корочки ржаного хлеба?",
+    'answerA':"А Токай",
+    'answerB':"B Кагор",
+    'answerC':"С Марсала",
+    'answerD':"D Херес",
+    'right':"A",
+    "fifty":"AC",
+},{
+    'question':"В каком городе в 1923 году был проведен первый международный кинофестиваль?",
+    'answerA':"А Канн   ",
+    'answerB':"B Венеция    ",
+    'answerC':"С Париж",
+    'answerD':"D Берлин",
+    'right':"B",
+    'fifty':"BA",
+},{
+    'question':"Чье произведение стало основой современного гимна Австрии?",
+    'answerA':"А Штраус",
+    'answerB':"B Шуберт",
+    'answerC':"С Шуман",
+    'answerD':"D Моцарт",
+    'right':"D",
+    'fifty':"DB",
+},{
+    'question':"Где проходила конференция, на которой Клара Цеткин предложила праздновать Международный женский день?",
+    'answerA':"А Лейпциг",
+    'answerB':"В Берн",
+    'answerC':"С Копенгаген",
+    'answerD':"D Берлин",
+    'right':"C",
+    'fifty':"CA",
+}];
+
+let prizes = [100,200,300,500,1000,2000,4000,8000,16000,32000,64000,125000,250000,500000,1000000];
+let questionField = document.querySelector('.question-container');
+let variantA = document.querySelector('.p-A');
+let variantB = document.querySelector('.p-B');
+let variantC = document.querySelector('.p-C');
+let variantD = document.querySelector('.p-D');
+let getPrize = document.querySelector('.score-count');
+let callButton = document.querySelector('.callButton');
+let fiftyButoon = document.querySelector('.fiftyButton');
+let tableRows = document.querySelectorAll('tr');
+let winString ;
+let loseSrting ;
+let questionNumber=1;
+let fiftyUsedWas=false;
+let callWasUsed=false;
+let temp = 0;
+let correct;
+let fiftyHelp;
+let prizeAmount=0;
+
+
+function AskEasy (){
+    let num=Math.floor(Math.random() * questionsE.length);
+    questionField.innerHTML= questionsE[num]["question"]; // change 0 to n
+    variantA.textContent = questionsE[num]['answerA'];  
+    variantB.textContent = questionsE[num]['answerB'];  
+    variantC.textContent = questionsE[num]['answerC'];  
+    variantD.textContent = questionsE[num]['answerD'];  
+    correct=questionsE[num]['right'];
+    fiftyHelp=questionsE[num]['fifty'];
+    questionsE.splice(num, 1); // deliting question
+}
+
+function AskMedium (){
+    let num=Math.floor(Math.random() * questionsM.length);
+    questionField.innerHTML= questionsM[num]["question"]; // change 0 to n
+    variantA.textContent = questionsM[num]['answerA'];  
+    variantB.textContent = questionsM[num]['answerB'];  
+    variantC.textContent = questionsM[num]['answerC'];  
+    variantD.textContent = questionsM[num]['answerD'];  
+    correct=questionsM[num]['right'];
+    fiftyHelp=questionsM[num]['fifty'];
+    questionsM.splice(num, 1); // deliting question
+}
+
+function AskHard (){
+    let num=Math.floor(Math.random() * questionsH.length);
+    questionField.innerHTML= questionsH[num]["question"]; // change 0 to n
+    variantA.textContent = questionsH[num]['answerA'];  
+    variantB.textContent = questionsH[num]['answerB'];  
+    variantC.textContent = questionsH[num]['answerC'];  
+    variantD.textContent = questionsH[num]['answerD'];  
+    correct=questionsH[num]['right'];
+    fiftyHelp=questionsH[num]['fifty'];
+    questionsH.splice(num, 1); // deliting question
+}
+
+function ChooseQuestion(questionNum){
+    if (questionNum<6){
+        AskEasy();
+    } else if((questionNum>=6)&&(questionNum<10)){
+        AskMedium();
+    } else if(questionNum>=10){
+        AskHard();
+    }
+}
+
+function prize(question){
+    prizeAmount=prizes[question-1];
+    getPrize.textContent = `Забрать выигрыш: ${prizeAmount}$`;
+    if(question==15){
+        question = 14;
+    }
+    tableRows[question].style.background="yellow";
+    if (question>0){
+    tableRows[question-1].style.background="white";}
+    if ((question>4)&&(question<10)){
+        tableRows[4].style.background="lightgreen";
+    }
+    if (question>9){
+        tableRows[4].style.background="white";
+        tableRows[9].style.background="lightgreen";
+    }
+}
+function GoHome(){
+    window.location.replace("index.html");
+}
+function WinFunc(){
+        winString=`Поздравляем, Вы выиграли ${prizeAmount}$`;
+        document.querySelector('.win').innerHTML = winString;
+        $("#win-modal").modal('show');
+}
+function LoseFunc(){
+    if (prizeAmount<1000){
+        loseSrting = "К сожалению Вы ничего не выиграли";
+    }else if((prizeAmount>=1000)&&(prizeAmount<32000)){
+        loseSrting = `Игра окончена, но Вы выиграли 1000$`;
+    }else if(prizeAmount>=32000){
+        loseSrting = "Игра окончена, но Вы выиграли 32000$";
+    }
+    document.querySelector('.lose').innerHTML = loseSrting;
+    $("#lose-modal").modal('show');
+}
+function FiftyHelpFunc(){
+    if (fiftyUsedWas==false){
+        for (let i=0;i<2;i++){
+            if (fiftyHelp[i]=="A"){
+                variantA.style.backgroundColor="orange";
+                
+            }
+            if (fiftyHelp[i]=="B"){
+                variantB.style.backgroundColor="orange";
+                
+            }
+            if (fiftyHelp[i]=="C"){
+                variantC.style.backgroundColor="orange";
+               
+            }
+            if (fiftyHelp[i]=="D"){
+                variantD.style.backgroundColor="orange";
+                
+            }
+        }
+        document.querySelector('.fiftyButton').setAttribute("disabled", "");
+        fiftyUsedWas=true
+    }
+}
+function CallFriendHelp(){
+    if (callWasUsed==false){ 
+        let x = Math.random() * 10;
+    if (x > 4){
+        if (correct[0]=="A"){
+            variantA.style.backgroundColor="orange";
+            
+        }
+        if (correct[0]=="B"){
+            variantB.style.backgroundColor="orange";
+            
+        }
+        if (correct[0]=="C"){
+            variantC.style.backgroundColor="orange";
+           
+        }
+        if (correct[0]=="D"){
+            variantD.style.backgroundColor="orange";
+            
+        }
+        console.log(x);
+    }else {
+        let y = Math.floor(Math.random() * 4);
+        if(y==1){
+            variantA.style.backgroundColor="orange";
+        }
+        if(y==2){
+            variantB.style.backgroundColor="orange";
+        }
+        if(y==3){
+            variantC.style.backgroundColor="orange";  
+        }
+        if(y==4){
+            variantD.style.backgroundColor="orange";
+        }
+        console.log(y);
+            
+    }
+    
+    document.querySelector('.callButton').setAttribute("disabled", "");
+    callWasUsed=true;
+}
+}
+function GetPrizeButtonAction(){
+    WinFunc();
+}
+ChooseQuestion(questionNumber);
+
+function eventForAnswerButton (ans){
+    if ((fiftyUsedWas==true)||(callWasUsed==true))
+        {variantA.style.backgroundColor="#198754";
+        variantB.style.backgroundColor="#198754";  
+        variantC.style.backgroundColor="#198754";
+        variantD.style.backgroundColor="#198754";}
+    if (ans==correct){
+        prize(questionNumber);
+        if (questionNumber==15){
+            WinFunc();
+           
+        }else{
+        questionNumber++;
+        ChooseQuestion(questionNumber);}
+    }else{
+        LoseFunc();
+        
+    } 
+
+}
+
+
